e258dc29f1c7ea7fccb71ad4c33da5221aac4779
Brayan-724 <bjqf0724@gmail.com>
2022-10-26 18:56:50 -0700

    pre

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,23 +1,7 @@
+import MaleAvatar from "../assets/undraw_male_avatar_re_hguk.svg"
+
 export default function IndexPage() {
   return (
-    <pre>{`                                                  
-                                                  
-                   ............                   
-               ....            ....               
-            ...                    ...            
-           ..                        ..           
-         ..            .. .            ..         
-        ..          ^JPGGPP55~          ..        
-        :          ?B#BBBBB##B:          :        
-       ..         .GBBBBBBG5J!:          ..       
-       ..          J#BBBBB?^^^^          ..       
-       ..          .5#YY57^^^^.          ..       
-        :            ??^^^^^:            :        
-        ..          ..^^^^^^:.          ..        
-         ..     .^!?JJJ77!!7JJ?7~:.    ..         
-           ..  ~JYYYYYYYYYYYYYYYYYJ~ ..           
-            ...JYYYYYYYYYYYYYYYYYYY7..            
-               :!?JYYYYYYYYYYYYJ?!:.              
-                  .:^~~!!!!~~^:.`}</pre>
+  <img src={MaleAvatar} />
   );
 }
